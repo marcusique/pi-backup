97383229eacd7aeb42c50abaa6a48f88b96563f0
marcusique <markajtay@gmail.com>
2020-11-13 23:48:02 +0100

    added elapsed time

--- pi-backup.sh
+++ pi-backup.sh
@@ -4,12 +4,16 @@
 # Define Variables
 ###################################
 
+# Elapsed time
+SECONDS=0
+
 # Storage device as defined in your /etc/fstab.
-mountpoint='/media/TimeMachine'
+MOUNTPOINT="/media/TimeMachine"
 
 # Path were the image of your SD card should be saved to
 STORAGEPATH="/media/TimeMachine/pi-backup"
 
+# Folder path for the most recent backup
 MOSTRECENTPATH="/media/TimeMachine/pi-backup/recent"
 
 # Image name
@@ -21,22 +25,22 @@ LOGFILE="/var/log/pi-backup.log"
 ###################################
 # Check Mount point Availability
 ###################################
+echo $(date +%Y-%m-%d_%H-%M-%S) ">>>>>BACKUP STARTED<<<<<"
 
-if [ "$(findmnt ${mountpoint})" ]; then
-    echo $(date +%Y-%m-%d_%H-%M-%S) " - The backup drive is accessible on "$HOSTNAME >>${LOGFILE}
+if [ "$(findmnt ${MOUNTPOINT})" ]; then
+    echo $(date +%Y-%m-%d_%H-%M-%S) " - The backup drive is available on "$HOSTNAME >>${LOGFILE}
 else
-    echo $(date +%Y-%m-%d_%H-%M-%S) " - Mount point was not accessible, please mind your device as defined in /etc/fstab" >>${LOGFILE}
+    echo $(date +%Y-%m-%d_%H-%M-%S) " - Error: mount point was not available, please mount your device as defined in /etc/fstab" >>${LOGFILE}
 
     #This command mounts all storages defined in /etc/fstab
     mount -a
 
     if [ $? != 0 ]; then
-        echo $(date +%Y-%m-%d_%H-%M-%S) " - The backup drive mounted" >>${LOGFILE}
+        echo $(date +%Y-%m-%d_%H-%M-%S) " - The backup drive mounted on "${MOUNTPOINT} >>${LOGFILE}
         sleep 5
         mount -a
         if [ $? != 0 ]; then
-            echo $(date +%Y-%m-%d_%H-%M-%S) " - Backup failed! The backup drive was not mounted. Please check it manually" >>${LOGFILE}
-            #echo "Sent backup status via e-mail" | mutt ${EMAIL} -a ${LOGFILE} -s $HOSTNAME" - Backup FAILED" >> ${LOGFILE}
+            echo $(date +%Y-%m-%d_%H-%M-%S) " - Error: the backup drive was not mounted. Please check it manually" >>${LOGFILE}
             exit
         fi
     fi
@@ -46,7 +50,7 @@ fi
 # Remove old Images from Storage Device
 ##################################################################
 
-echo $(date +%Y-%m-%d_%H-%M-%S) " - Starting to delete files older than 7 days" >>${LOGFILE}
+echo $(date +%Y-%m-%d_%H-%M-%S) " - Starting to delete files older than 7 days in ${STORAGEPATH}" >>${LOGFILE}
 
 # Remove files older than 7 days
 find ${STORAGEPATH}/*.* -mtime +7 -exec rm -r {} \;
@@ -54,7 +58,7 @@ find ${STORAGEPATH}/*.* -mtime +7 -exec rm -r {} \;
 if [ $? != 0 ]; then
     echo $(date +%Y-%m-%d_%H-%M-%S) " - Old images successfully removed" >>${LOGFILE}
     if [ $? != 0 ]; then
-        echo $(date +%Y-%m-%d_%H-%M-%S) " - Was not able to delete old image files. You have to check it manually" >>${LOGFILE}
+        echo $(date +%Y-%m-%d_%H-%M-%S) " - Error: was not able to delete old image files in ${STORAGEPATH}. Please check manually" >>${LOGFILE}
         break
     fi
 fi
@@ -63,12 +67,12 @@ fi
 # Clone SD Card Image
 ###################################
 
-echo $(date +%Y-%m-%d_%H-%M-%S) " - Started to clone pi image" >>${LOGFILE}
+echo $(date +%Y-%m-%d_%H-%M-%S) " - Started to clone pi image to "${STORAGEPATH}/${IMAGENAME}-$(date +%Y-%m-%d).img >>${LOGFILE}
 
 # Saves a plain img file
 sudo dd if=/dev/mmcblk0 of=${STORAGEPATH}/${IMAGENAME}-$(date +%Y-%m-%d).img bs=8MB
 
-echo $(date +%Y-%m-%d_%H-%M-%S) " - Finished to clone image" >>${LOGFILE}
+echo $(date +%Y-%m-%d_%H-%M-%S) " - Finished to clone image to "${STORAGEPATH}/${IMAGENAME}-$(date +%Y-%m-%d).img >>${LOGFILE}
 
 ###################################
 # Resize dd Image
@@ -77,28 +81,30 @@ echo $(date +%Y-%m-%d_%H-%M-%S) " - Finished to clone image" >>${LOGFILE}
 # Please see https://github.com/Drewsif/PiShrink for further details
 # pishrink.sh must be located in the same directory as this script!
 
-echo $(date +%Y-%m-%d_%H-%M-%S) " - Started to compress the image" >>${LOGFILE}
+echo $(date +%Y-%m-%d_%H-%M-%S) " - Started to compress the image to "${STORAGEPATH}/${IMAGENAME}-$(date +%Y-%m-%d).img >>${LOGFILE}
 sudo /bin/bash /usr/local/bin/pishrink.sh -d ${STORAGEPATH}/${IMAGENAME}-$(date +%Y-%m-%d).img ${STORAGEPATH}/${IMAGENAME}_$(date +%Y-%m-%d)-compressed.img
-echo $(date +%Y-%m-%d_%H-%M-%S) " - Finished compressing the image" >>${LOGFILE}
+echo $(date +%Y-%m-%d_%H-%M-%S) " - Finished compressing the image to "${STORAGEPATH}/${IMAGENAME}_$(date +%Y-%m-%d)-compressed.img >>${LOGFILE}
 
 #Delete big image file
-echo $(date +%Y-%m-%d_%H-%M-%S) " - Started to delete the original image" >>${LOGFILE}
+echo $(date +%Y-%m-%d_%H-%M-%S) " - Started to delete the original image "${STORAGEPATH}/${IMAGENAME}-$(date +%Y-%m-%d).img >>${LOGFILE}
 sudo rm ${STORAGEPATH}/${IMAGENAME}-$(date +%Y-%m-%d).img
-echo $(date +%Y-%m-%d_%H-%M-%S) " - Deleted the original image" >>${LOGFILE}
+echo $(date +%Y-%m-%d_%H-%M-%S) " - Deleted the original image "${STORAGEPATH}/${IMAGENAME}-$(date +%Y-%m-%d).img >>${LOGFILE}
 
 ###################################
 # Move the backup to the recent folder
 ###################################
 
 #Remove files from recent folder
-echo $(date +%Y-%m-%d_%H-%M-%S) " - Purging the recent folder" >>${LOGFILE}
+echo $(date +%Y-%m-%d_%H-%M-%S) " - Purging the recent folder "${MOSTRECENTPATH} >>${LOGFILE}
 find ${MOSTRECENTPATH}/*.img -exec rm -r {} \;
-echo $(date +%Y-%m-%d_%H-%M-%S) " - The recent folder has been purged" >>${LOGFILE}
+echo $(date +%Y-%m-%d_%H-%M-%S) " - The recent folder has been purged "${MOSTRECENTPATH} >>${LOGFILE}
 
 #Move the backup to the recent folder
-echo $(date +%Y-%m-%d_%H-%M-%S) " - Moving backup to the recent folder" >>${LOGFILE}
+echo $(date +%Y-%m-%d_%H-%M-%S) " - Moving backup to "${MOSTRECENTPATH}/${IMAGENAME}-recent.img >>${LOGFILE}
 sudo mv ${STORAGEPATH}/${IMAGENAME}_$(date +%Y-%m-%d)-compressed.img ${MOSTRECENTPATH}/${IMAGENAME}-recent.img
-echo $(date +%Y-%m-%d_%H-%M-%S) " - Backup has been moved to the recent folder" >>${LOGFILE}
+echo $(date +%Y-%m-%d_%H-%M-%S) " - Backup has been moved to "${MOSTRECENTPATH}/${IMAGENAME}-recent.img >>${LOGFILE}
 
 # Script finished
-echo $(date +%Y-%m-%d_%H-%M-%S) " - Backup successful" >>${LOGFILE}
+duration=$SECONDS
+echo $(date +%Y-%m-%d_%H-%M-%S) " - Backup completed in ${duration} seconds" >>${LOGFILE}
+echo $(date +%Y-%m-%d_%H-%M-%S) ">>>>>BACKUP FINISHED<<<<<"
